dd1d6396984ad26bcdc724bb3fbd760d8d4b9e67
xiaoxu.gao <highsmallxu@gmail.com>
2018-09-07 15:05:54 +0200

    qrcode

--- client/client/FirstViewController.swift
+++ client/client/FirstViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class FirstViewController: UIViewController {
 
@@ -19,6 +20,70 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBOutlet weak var imgQRCode: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
+    var qrcodeImage: CIImage!
+
+
+    @IBAction func button(_ sender: UIButton) {
+        
+        if(qrcodeImage==nil){
+            
+            let data = textField.text?.data(using: String.Encoding.isoLatin1, allowLossyConversion: false)
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            filter?.setValue(data, forKey: "inputMessage")
+            filter?.setValue("Q", forKey: "inputCorrectionLevel")
+            
+            qrcodeImage = filter?.outputImage
+            
+            textField.resignFirstResponder()
+            
+            displayQRCodeImage()
+            
+            
+        }
+
+        
+        
+        
+        var uuid = UUID().uuidString;
+        var ref:DatabaseReference!
+        ref = Database.database().reference()
+        ref.child("merchant")
+            .child("merchant1")
+            .setValue(["stampId":uuid]){
+                (error:Error?, ref:DatabaseReference) in
+                if let error = error {
+                    print(error)
+                    print("Data could not be saved")
+                } else {
+                    print("Data saved sucessfully")
+                }
+        }
+        
+        
+        
+        
+        
+//        ref.child("merchant").setValue(["merchant1":uuid])
+        print(uuid)
+    }
+    
+    func displayQRCodeImage() {
+        let scaleX = imgQRCode.frame.size.width / qrcodeImage.extent.size.width
+        let scaleY = imgQRCode.frame.size.height / qrcodeImage.extent.size.height
+        
+        let transformedImage = qrcodeImage.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
+        
+        imgQRCode.image = UIImage(ciImage: transformedImage)
+    }
+    
+    
+    
+    
 
 
 }
